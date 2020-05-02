14b1dc095fbe107472f9dd7e3fcef55c4bc1da99
Dev Boldly <devboldly@gmail.com>
2020-05-02 15:56:01 -0500

    Update documentation

--- src/__tests__/BuildStatusBadge.test.tsx
+++ src/__tests__/BuildStatusBadge.test.tsx
@@ -2,9 +2,6 @@ import { render, RenderResult } from '@testing-library/react';
 import React from 'react';
 import { BuildStatusBadge, BuildStatusBadgeProps } from '../components/BuildStatusBadge';
 
-// Learn how to test React components:
-// https://testing-library.com/docs/react-testing-library/intro
-
 function renderComponent(props: BuildStatusBadgeProps, children: React.ReactNode = undefined): RenderResult {
   return render(<BuildStatusBadge {...props}>{children}</BuildStatusBadge>);
 }
