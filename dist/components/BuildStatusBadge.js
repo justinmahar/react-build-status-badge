d9a10a13f3c8cdfe16aa582a21b19203cdf23625
Justin Mahar <contact@justinmahar.com>
2021-02-28 17:14:32 -0600

    Build, version, and publish.

--- dist/components/BuildStatusBadge.js
+++ dist/components/BuildStatusBadge.js
@@ -1,31 +1,36 @@
 "use strict";
-var __assign =
-  (this && this.__assign) ||
-  function() {
-    __assign =
-      Object.assign ||
-      function(t) {
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
-          s = arguments[i];
-          for (var p in s)
-            if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
         }
         return t;
-      };
+    };
     return __assign.apply(this, arguments);
-  };
-var __importStar =
-  (this && this.__importStar) ||
-  function(mod) {
+};
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null)
-      for (var k in mod)
-        if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
     return result;
-  };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.BuildStatusBadge = void 0;
 var React = __importStar(require("react"));
 var react_timed_image_1 = require("react-timed-image");
 /**
@@ -40,64 +45,51 @@ var react_timed_image_1 = require("react-timed-image");
  * See the props section for more options.
  */
 function BuildStatusBadge(props) {
-  var href = props.href;
-  var src = props.src;
-  var alt = props.alt;
-  if (typeof props.children === "string") {
-    // Regex matches on: [![alt](src)](href)
-    // It looks more complicated that it is, because there's so much [] and () escaping needed.
-    // Groups are named by putting ?<name> immediately after the opening paren.
-    var imageLinkRegex = /\[!\[(?<alt>[^\]]*)\]\((?<src>[^)]+)\)\]\((?<href>[^)]+)\)/;
-    var match = props.children.match(imageLinkRegex);
-    if (match) {
-      var groups = match.groups;
-      if (groups) {
-        href = groups.href;
-        src = groups.src;
-        alt = groups.alt;
-      }
-    } else {
-      // Regex matches on: ![alt](src)
-      // (Just an image with no link)
-      var imageRegex = /!\[(?<alt>[^\]]*)\]\((?<src>[^)]+)\)/;
-      var match_1 = props.children.match(imageRegex);
-      if (match_1) {
-        var groups = match_1.groups;
-        if (groups) {
-          src = groups.src;
-          alt = groups.alt;
+    var href = props.href;
+    var src = props.src;
+    var alt = props.alt;
+    if (typeof props.children === 'string') {
+        // Regex matches on: [![alt](src)](href)
+        // It looks more complicated that it is, because there's so much [] and () escaping needed.
+        // Groups are named by putting ?<name> immediately after the opening paren.
+        var imageLinkRegex = /\[!\[(?<alt>[^\]]*)\]\((?<src>[^)]+)\)\]\((?<href>[^)]+)\)/;
+        var match = props.children.match(imageLinkRegex);
+        if (match) {
+            var groups = match.groups;
+            if (groups) {
+                href = groups.href;
+                src = groups.src;
+                alt = groups.alt;
+            }
         }
-      }
+        else {
+            // Regex matches on: ![alt](src)
+            // (Just an image with no link)
+            var imageRegex = /!\[(?<alt>[^\]]*)\]\((?<src>[^)]+)\)/;
+            var match_1 = props.children.match(imageRegex);
+            if (match_1) {
+                var groups = match_1.groups;
+                if (groups) {
+                    src = groups.src;
+                    alt = groups.alt;
+                }
+            }
+        }
     }
-  }
-  var useLink = !props.linkDisabled && typeof href !== "undefined";
-  var imageElement = !props.reloadDisabled
-    ? React.createElement(react_timed_image_1.TimedImage, {
-        interval:
-          typeof props.interval === "number" && props.interval > 0
-            ? Math.abs(props.interval)
-            : 5000,
-        src: src,
-        alt: alt,
-      })
-    : React.createElement("img", { src: src, alt: alt });
-  if (useLink) {
-    var newWindowProps = props.openInNewWindow
-      ? { target: "_blank", rel: "noopener noreferrer" }
-      : {};
-    return React.createElement(
-      "a",
-      __assign({ href: href }, newWindowProps),
-      imageElement
-    );
-  } else {
-    return imageElement;
-  }
+    var useLink = !props.linkDisabled && typeof href !== 'undefined';
+    var imageElement = !props.reloadDisabled ? (React.createElement(react_timed_image_1.TimedImage, { interval: typeof props.interval === 'number' && props.interval > 0 ? Math.abs(props.interval) : 5000, src: src, alt: alt })) : (React.createElement("img", { src: src, alt: alt }));
+    if (useLink) {
+        var newWindowProps = props.openInNewWindow ? { target: '_blank', rel: 'noopener noreferrer' } : {};
+        return (React.createElement("a", __assign({ href: href }, newWindowProps), imageElement));
+    }
+    else {
+        return imageElement;
+    }
 }
 exports.BuildStatusBadge = BuildStatusBadge;
 BuildStatusBadge.defaultProps = {
-  interval: 5000,
-  reloadDisabled: false,
-  linkDisabled: false,
-  openInNewWindow: false,
+    interval: 5000,
+    reloadDisabled: false,
+    linkDisabled: false,
+    openInNewWindow: false,
 };
